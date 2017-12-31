b294a1f16540dbcad5ffc7fa221d68f4c7341521
Tito Yslas <roberto.yslas@gmail.com>
2017-12-30 21:25:24 -0700

    oop - intro to classes, pt 2

--- 1-classes-pt1/src/com/tito/Car.java
+++ 1-classes-pt1/src/com/tito/Car.java
@@ -2,12 +2,29 @@ package com.tito;
 
 /* it is convention to capitalize the first letter of a Java class */
 public class Car {
-  // below defines the "state" of the car, defined by "fields"
+  // below defines the "state" of the Car class, defined by "fields"
   private int doors; // not accessible outside of this class
   private int wheels;
-  private String model;
+   private String model; // Strings bend the rules of Java since they are technically classes
   private String engine;
   private String color;
 
+  /* the public methods below define the "behavior" of the Car class */
+  // below is an example of a "setter"
+  public void setModel(String model) {
+    // add validation functionality within its own class
+    // this serves to prevent the Car class from creating invalid objects
+    // allows our code to validate that the user to properly using the application
+    String validModel = model.toLowerCase();
+    if (validModel.equals("911 turbo") || validModel.equals("commodore")) {
+      this.model = model;
+    } else {
+      this.model = "unknown";
+    }
+  }
 
+  // below is an example of a "getter"
+  public String getModel() {
+    return this.model;
+  }
 }
--- 1-classes-pt1/src/com/tito/Main.java
+++ 1-classes-pt1/src/com/tito/Main.java
@@ -7,6 +7,9 @@ public class Main {
     // ⬆ creates new data type called porsche of type "Car"
     Car holden = new Car();
     // ⬆ creates new object called holden based on the Car template
-    porsche.model = "911 Turbo";
+
+    porsche.setModel("911 Turbo"); // call public method to change private field
+    System.out.println("the model is a/an " + porsche.getModel());
+
   }
 }
